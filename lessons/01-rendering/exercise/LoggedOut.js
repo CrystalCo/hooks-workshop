ffb56e57f1b5e8925adea06dc23656714f080347
Crystal Contreras <crystal.contr@gmail.com>
2019-05-02 09:54:07 -0500

    Exercise 1

--- lessons/01-rendering/exercise/LoggedOut.js
+++ lessons/01-rendering/exercise/LoggedOut.js
@@ -6,6 +6,28 @@ import About from "app/About"
 // import LoggedOut from "./LoggedOut.final"
 // export default LoggedOut
 
+// Has Aria accessibility.  Can use left and right arrow keys to switch tabs
+
 export default function LoggedOut() {
-  return null
+  return (
+    <div className="LoggedOut">
+      <About />
+      <Tabs>
+        <TabList>
+          <Tab>
+            Sign Up
+          </Tab>
+          <Tab>Login</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <SignupForm />
+          </TabPanel>
+          <TabPanel>
+            <LoginForm />
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </div>
+  )
 }
