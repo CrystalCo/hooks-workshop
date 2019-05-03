c740a64ae48988979a1c7e7de5ca4dadc129195f
Crystal Contreras <crystal.contr@gmail.com>
2019-05-03 16:02:53 -0500

    Notes on optimization

--- lessons/11-animation/lecture/AnimatedDialog.js
+++ lessons/11-animation/lecture/AnimatedDialog.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react"
+import React, { useRef, useEffect, memo } from "react"
 import { DialogOverlay, DialogContent } from "@reach/dialog"
 import { useTransition, animated } from "react-spring"
 import { FaTimes } from "react-icons/fa"
@@ -74,52 +74,52 @@ export default function AnimatedDialog(props) {
 // a lot. Bailing out of react's updates makes sense here because for
 // animations we really want at solid frames per second.
 
-// animated.DialogOverlay = animated(DialogOverlay)
-// animated.DialogContent = animated(DialogContent)
+animated.DialogOverlay = animated(DialogOverlay)
+animated.DialogContent = animated(DialogContent)
 
-// export default function AnimatedDialog(props) {
-//   const rootRef = useRef(null)
-//   if (!rootRef.current) {
-//     rootRef.current = document.getElementById("root")
-//   }
+export default memo(function AnimatedDialog(props) {
+  const rootRef = useRef(null)
+  if (!rootRef.current) {
+    rootRef.current = document.getElementById("root")
+  }
 
-//   const transitions = useTransition(props.isOpen ? props : false, null, {
-//     from: { opacity: 0, y: -10, blur: 0 },
-//     enter: { opacity: 1, y: 0, blur: 8 },
-//     leave: { opacity: 0, y: 10, blur: 0 }
-//   })
+  const transitions = useTransition(props.isOpen ? props : false, null, {
+    from: { opacity: 0, y: -10, blur: 0 },
+    enter: { opacity: 1, y: 0, blur: 8 },
+    leave: { opacity: 0, y: 10, blur: 0 }
+  })
 
-//   const blur = props.isOpen ? 8 : 0
+  const blur = props.isOpen ? 8 : 0
 
-//   useEffect(() => {
-//     rootRef.current.style.filter = `blur(${blur}px)`
-//   }, [blur])
+  useEffect(() => {
+    rootRef.current.style.filter = `blur(${blur}px)`
+  }, [blur])
 
-//   return transitions.map(
-//     ({ item, key, props: { opacity, y } }) =>
-//       item.isOpen && (
-//         <animated.DialogOverlay
-//           key={key}
-//           style={{ opacity }}
-//           onDismiss={item.onDismiss}
-//         >
-//           <animated.DialogContent
-//             style={{
-//               transform: y.interpolate(y => `translate3d(0px, ${y}px, 0px)`)
-//             }}
-//           >
-//             {item.children}
-//             <button
-//               className="Dialog_close icon_button"
-//               onClick={item.onDismiss}
-//             >
-//               <FaTimes aria-label="Close new post dialog" />
-//             </button>
-//           </animated.DialogContent>
-//         </animated.DialogOverlay>
-//       )
-//   )
-// }
+  return transitions.map(
+    ({ item, key, props: { opacity, y } }) =>
+      item.isOpen && (
+        <animated.DialogOverlay
+          key={key}
+          style={{ opacity }}
+          onDismiss={item.onDismiss}
+        >
+          <animated.DialogContent
+            style={{
+              transform: y.interpolate(y => `translate3d(0px, ${y}px, 0px)`)
+            }}
+          >
+            {item.children}
+            <button
+              className="Dialog_close icon_button"
+              onClick={item.onDismiss}
+            >
+              <FaTimes aria-label="Close new post dialog" />
+            </button>
+          </animated.DialogContent>
+        </animated.DialogOverlay>
+      )
+  )
+})
 
 /******************************************************************************/
 // Lets animate the blur on the background. Since the root element isn't owned
@@ -127,49 +127,50 @@ export default function AnimatedDialog(props) {
 // called for every frame of the animation, allowing us to synchronize this
 // animation to things outside of this component
 
-// animated.DialogOverlay = animated(DialogOverlay)
-// animated.DialogContent = animated(DialogContent)
+// Wrappers from react-spring. Conventional for react-spring
+animated.DialogOverlay = animated(DialogOverlay)
+animated.DialogContent = animated(DialogContent)
 
-// export default function AnimatedDialog(props) {
-//   const rootRef = useRef(null)
-//   if (!rootRef.current) {
-//     rootRef.current = document.getElementById("root")
-//   }
+export default function AnimatedDialog(props) {
+  const rootRef = useRef(null)
+  if (!rootRef.current) {
+    rootRef.current = document.getElementById("root")
+  }
+  // useTransitions hands you transitions. hangs on to all props to let you render the calendar
+  const transitions = useTransition(props.isOpen ? props : false, null, {
+    from: { opacity: 0, y: -10, blur: 0 },
+    enter: { opacity: 1, y: 0, blur: 8 },
+    leave: { opacity: 0, y: 10, blur: 0 },
+    onFrame: (item, state, { blur }) => {
+      if (item.isOpen) rootRef.current.style.filter = `blur(${blur}px)`
+    }
+  })
 
-//   const transitions = useTransition(props.isOpen ? props : false, null, {
-//     from: { opacity: 0, y: -10, blur: 0 },
-//     enter: { opacity: 1, y: 0, blur: 8 },
-//     leave: { opacity: 0, y: 10, blur: 0 },
-//     onFrame: (item, state, { blur }) => {
-//       if (item.isOpen) rootRef.current.style.filter = `blur(${blur}px)`
-//     }
-//   })
-
-//   return transitions.map(
-//     ({ item, key, props: { opacity, y } }) =>
-//       item.isOpen && (
-//         <animated.DialogOverlay
-//           key={key}
-//           style={{ opacity }}
-//           onDismiss={item.onDismiss}
-//         >
-//           <animated.DialogContent
-//             style={{
-//               transform: y.interpolate(y => `translate3d(0px, ${y}px, 0px)`)
-//             }}
-//           >
-//             {item.children}
-//             <button
-//               className="Dialog_close icon_button"
-//               onClick={item.onDismiss}
-//             >
-//               <FaTimes aria-label="Close new post dialog" />
-//             </button>
-//           </animated.DialogContent>
-//         </animated.DialogOverlay>
-//       )
-//   )
-// }
+  return transitions.map(
+    ({ item, key, props: { opacity, y } }) =>
+      item.isOpen && (
+        <animated.DialogOverlay
+          key={key}
+          style={{ opacity }}
+          onDismiss={item.onDismiss}
+        >
+          <animated.DialogContent
+            style={{
+              transform: y.interpolate(y => `translate3d(0px, ${y}px, 0px)`)
+            }}
+          >
+            {item.children}
+            <button
+              className="Dialog_close icon_button"
+              onClick={item.onDismiss}
+            >
+              <FaTimes aria-label="Close new post dialog" />
+            </button>
+          </animated.DialogContent>
+        </animated.DialogOverlay>
+      )
+  )
+}
 
 /******************************************************************************/
 // Alright, now lets look at adding transitions between two different elements
--- lessons/12-optimization/lecture/README.md
+++ lessons/12-optimization/lecture/README.md
@@ -5,6 +5,17 @@
 - useMemo on calculateWeeks.js
   - console.time it
   - console.log when it memos
+  - use devTools Performance tab to see how fast or slow it is, and if slower than 30ms after 6x slower, useMemo to optimize:
+  # useMemo: only rerender if state changes
+  const weeks = useMemo(() => {
+      // it's a calculation instead of an effect
+      return calculateWeeks(posts, startDate, numWeeks)
+  }, [posts, startData, numWeeks])
+
+  # use callBacks
+  const handleTabChange = useCallback   === const handleTabChange = () =>
+
+  
 - Go to the feed
 - Show how the profiler works
 - React.memo on FeedPost and show how it saves on renders
